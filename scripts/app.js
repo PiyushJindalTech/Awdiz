8f9943a714e17b6b60b267bc847a09a8425c1ab3
PiyushJindal <piyush95ksp@gmail.com>
2023-01-11 21:21:04 +0530

    login page added

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+const btnLogin = document.getElementById('btnLogin');
+const txtEmail = document.getElementById('email');
+const txtPassword = document.getElementById('password');
+
+
+
+function onLogin() {
+    const emailId = txtEmail.value;
+    const password = txtPassword.value;
+
+    if (emailId.trim() === "" || password.trim() === "") {
+        alert("Invalid Email Id or Password");
+        return;
+    }
+    alert('logged In!');
+}
+
+function onEditEmail(event) {
+    if (isNaN(parseInt(event.key)) === true) {
+        txtEmail.style.borderColor = 'red';
+    }
+    else {
+        txtEmail.style.borderColor = 'black';
+    }
+    console.log(event.key);
+    if (parseInt(event.key))
+        console.log("onEditEmail");
+}
+
+btnLogin.addEventListener("click", onLogin);
+txtEmail.addEventListener("keypress", onEditEmail);
+
+
+console.log(btnLogin);
+
